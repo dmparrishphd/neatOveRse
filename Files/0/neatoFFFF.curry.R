bf8b13a336b74d97024406cf4ac65852fbad372a
dmparrishphd <39537744+dmparrishphd@users.noreply.github.com>
2020-10-17 17:26:02 -0400

    Delete neatoFFFF.curry.R

--- Files/0/neatoFFFF.curry.R
+++
@@ -1,9 +0,0 @@
-curry <- function(FUN, ...) {
-    #REMINDER COMMENTS #1, #2, #3, AND #4 ARE REFERENCED IN THE LICENCE FILE
-    .orig <- match.call() #1
-    .orig[[1]] <- NULL #2 Remove first item, which matches FUN
-    .orig[[1]] <- NULL #2 Remove second item, which matches the Curried argument
-    function(...) {
-        .inner <- match.call()
-        .inner[[1]] <- NULL # Remove first item, which matches curry #3
-        do.call(FUN, c(.orig, .inner), envir=parent.frame()) } } #4
