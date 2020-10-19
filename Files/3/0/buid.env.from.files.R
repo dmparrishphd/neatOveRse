8a7396e6c9c4695d13dd28a37022e2e4d6947154
dmparrishphd <39537744+dmparrishphd@users.noreply.github.com>
2020-10-18 23:46:33 -0400

    Update buid.env.from.files.R

--- Files/3/0/buid.env.from.files.R
+++ Files/3/0/buid.env.from.files.R
@@ -1,4 +1,4 @@
-buid.env.from.files <- function(parent.name, path, files) {
+build.env.from.files <- function(parent.name, path, files) {
     Envir <- new.env(parent=as.environment(parent.name))
     FILENAMES <- paste0(path, files)
     for (Filename in FILENAMES) source(Filename, local=Envir)
