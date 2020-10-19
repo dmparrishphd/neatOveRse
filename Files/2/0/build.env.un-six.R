02997aa6b7b5ed962e99a07c9046a75f7aa517b1
dmparrishphd <39537744+dmparrishphd@users.noreply.github.com>
2020-10-18 22:06:49 -0400

    Create build.env.un-six.R

---
+++ Files/2/0/build.env.un-six.R
@@ -0,0 +1,4 @@
+attach(name="FunProgEnv", what=build.env(
+	  parent.name="package:stats",
+	  path="~/Mirrors/GitHub/dmparrishphd/neatOveRse/Pkgs/",
+	  subdirs=strsplit("1/0 2/0 3/0 4/0 5/0 6/0", " ")[[1]]))
