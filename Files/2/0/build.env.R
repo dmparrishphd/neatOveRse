827c68dfef27276a882f3d22391d63a7775b12d8
dmparrishphd <39537744+dmparrishphd@users.noreply.github.com>
2020-10-19 00:47:58 -0400

    Update build.env.R

--- Files/2/0/build.env.R
+++ Files/2/0/build.env.R
@@ -1,5 +1,5 @@
 build.env <- function(parent.name, path, subdirs, pattern="[.]R$") {
     Envir <- new.env(parent=as.environment(parent.name))
     FILENAMES <- list.files(path, pattern=pattern, recursive=T)
-    for (Filename in FILENAMES) source(Filename, local=Envir)
+    for (Filename in FILENAMES) source(paste0(path, Filename), local=Envir)
     Envir }
