c1e119d7735dff9284b7483969aa8ffee543950a
dmparrishphd <39537744+dmparrishphd@users.noreply.github.com>
2020-10-19 21:07:59 -0400

    Create build.env.build.R

---
+++ Files/3/0/build.env.build.R
@@ -0,0 +1,9 @@
+(function(mirrors) {
+    Envir <- new.env(parent=as.environment("package:stats"))
+    `%//%` <- paste0
+    NEATOVERSE <- mirrors %//% "GitHub/dmparrishphd/neatOveRse/" 
+    for (Filename in c(
+        NEATOVERSE %//% "Files/2/0/build.env.R",
+        NEATOVERSE %//% "Files/3/0/build.env.from.files.R"))
+            source(Filename, local=Envir)
+    attach(name="buildEnv", what=Envir) })
