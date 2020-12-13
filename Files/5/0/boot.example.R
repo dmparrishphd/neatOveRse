4deab74cf9ecb205681b02d2cca5562aac64c795
dmparrishphd <39537744+dmparrishphd@users.noreply.github.com>
2020-12-13 15:13:41 -0500

    Add files via upload

---
+++ Files/5/0/boot.example.R
@@ -0,0 +1,8 @@
+function(mirrors) {
+    `%//%` <- paste0
+    DIR <- mirrors %//% "GitHub/dmparrishphd/neatOveRse/Files/"
+    source(DIR %//% "3/0/boot.R")[[1]](
+        mirrors=mirrors, path=DIR, files=c(
+            "4/0/build.env.build.R",
+            "3/0/build.env.tRivia1.R",
+            "3/0/build.env.un-six.R"))}
