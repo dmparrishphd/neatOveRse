05d22b6f9ba5c4358113d754f8b7ea00a37529bd
dmparrishphd <39537744+dmparrishphd@users.noreply.github.com>
2021-03-08 20:19:41 -0500

    Update adjacent.R

--- Dev/0/adjacent.R
+++ Dev/0/adjacent.R
@@ -7,3 +7,18 @@ adjacent <- function ( mi ) {
 	dxy <- mi [ i [ , 1 ] , ] - mi [ i [ , 2 ] , ]
 	matrix ( nrow = n , ncol = n , data =
 		abs ( dxy [ , 1 ] + dxy [ , 2 ] ) == 1L ) }
+
+
+whichAdjacent <- function ( mi ) {
+	ADJ <- adjacent ( mi )
+	n <- nrow ( ADJ )
+	i <- as.vector ( row ( ADJ ) )
+	j <- as.vector ( col ( ADJ ) )
+	ij <- vapply ( as.data.frame ( optional = TRUE , rbind ( i , j ) ) , sort , integer(2) )
+	rownames ( ij ) <- c ( "i" , "j" )
+	DF <- data.frame ( flag = as.vector ( ADJ ), t ( ij ) )
+	DF <- DF [ DF $ flag , ]
+	rownames ( DF ) <- NULL
+	as.matrix ( DF [ -1 ] [ ! duplicated ( DF [ -1 ] ) , ] ) }
+
+
